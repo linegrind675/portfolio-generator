9ce9edc9b8f4558593b6fdedbe39de1eb2778205
Linegrind <linegrind@icloud.com>
2021-03-18 16:32:34 -0600

    create an HTML template

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
 
-console.log(profileDataArgs);
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = (profileDataArr) => {
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error (err);
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
+
+// const printProfileData = (profileDataArr) => {
     
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
     
-};
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
