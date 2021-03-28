ca8e5f4e284e4fae8d88e7c76c5ad3a3074ec334
Linegrind <linegrind@icloud.com>
2021-03-28 15:20:09 -0600

    finishing the site with promises

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
+
 
 const promptUser = () => {
 
@@ -141,17 +142,26 @@ Add a New Project
 promptUser()
 
     .then(promptProject)
-
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
+        return generatePage(portfolioData);
+    })
 
-        fs.writeFile('./index.html', pageHTML, err => {
-            if (err) throw new Error(err);
+    .then(pageHTML => {
+        return writeFile(pageHTML);    
+    })
 
-            console.log('Page created! Check out index.html in this directory to see it!');
-        });
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);  
+    }) 
+
+    .catch(err => {
+        console.log(err);
     });
-
 
 
 
