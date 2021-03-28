2a0be733bf2d9e0bb9acec8032707007768ee0f6
Linegrind <linegrind@icloud.com>
2021-03-28 14:26:06 -0600

    extra commit

--- app.js
+++ app.js
@@ -143,7 +143,13 @@ promptUser()
     .then(promptProject)
 
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+
+            console.log('Page created! Check out index.html in this directory to see it!');
+        });
     });
 
 
